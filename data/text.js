cc8edf174f4341e6ae75ad757eda40f1b0d27c3d
n8chz <n8chz@yahoo.ca>
2015-04-26 19:46:13 -0400

    now matching against URI and title, as well as text

--- data/text.js
+++ data/text.js
@@ -1,7 +1,7 @@
 self.port.on("getText", function () {
   if (document.body) {
    self.port.emit("gotText", JSON.stringify({
-     url: document.location.href,
+     url: document.location.href, // "document.location is null"
      title: document.title,
      text: document.body.textContent
    }));
